5bef0a3f4b0115222c0c8707751874fac1d9cc97
Ian Abbott <ij.abbott2306@gmail.com>
2018-10-12 01:20:27 -0500

    Adding tests for more roman to decimal patterns, all passed on the first try

--- check_roman_to_decimal.c
+++ check_roman_to_decimal.c
@@ -3,12 +3,30 @@
 #include "check_roman_to_decimal.h"
 #include "numeral_converter.h"
 
+/* Cases Covered
+	- Each individual numearl
+	- Repeated numerals
+	- Lesser numeral before larger numeral
+	- Lesser numeral after larger numeral
+	- Lesser numeral between two larger numerals
+	- Maximum numeral 
+*/
+
 START_TEST(test_convert_I_to_1)
 {
 	int decimal = 0;
 	
 	numeral_to_decimal("I", &decimal);
 	ck_assert_int_eq(1, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_II_to_2)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("II", &decimal);
+	ck_assert_int_eq(2, decimal);
 }
 END_TEST
 
@@ -30,6 +48,15 @@ START_TEST(test_convert_V_to_5)
 }
 END_TEST
 
+START_TEST(test_convert_VI_to_6)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("VI", &decimal);
+	ck_assert_int_eq(6, decimal);
+}
+END_TEST
+
 START_TEST(test_convert_IX_to_9)
 {
 	int decimal = 0;
@@ -48,12 +75,93 @@ START_TEST(test_convert_X_to_10)
 }
 END_TEST
 
+START_TEST(test_convert_XI_to_11)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("XI", &decimal);
+	ck_assert_int_eq(11, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_XIV_to_14)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("XIV", &decimal);
+	ck_assert_int_eq(14, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_XV_to_15)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("XV", &decimal);
+	ck_assert_int_eq(15, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_XVI_to_16)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("XVI", &decimal);
+	ck_assert_int_eq(16, decimal);
+}
+END_TEST
+
 START_TEST(test_convert_XL_to_40)
 {
 	int decimal = 0;
 	
 	numeral_to_decimal("XL", &decimal);
 	ck_assert_int_eq(40, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_L_to_50)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("L", &decimal);
+	ck_assert_int_eq(50, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_C_to_100)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("C", &decimal);
+	ck_assert_int_eq(100, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_D_to_500)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("D", &decimal);
+	ck_assert_int_eq(500, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_M_to_1000)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("M", &decimal);
+	ck_assert_int_eq(1000, decimal);
+}
+END_TEST
+
+START_TEST(test_convert_MMMCMXCIX_to_3999)
+{
+	int decimal = 0;
+	
+	numeral_to_decimal("MMMCMXCIX", &decimal);
+	ck_assert_int_eq(3999, decimal);
 }
 END_TEST
 
@@ -67,11 +175,22 @@ Suite * roman_to_decimal_suite(void)
 	tc_core = tcase_create("Core");
 
 	tcase_add_test(tc_core, test_convert_I_to_1);
+	tcase_add_test(tc_core, test_convert_II_to_2);
 	tcase_add_test(tc_core, test_convert_IV_to_4);
 	tcase_add_test(tc_core, test_convert_V_to_5);
+	tcase_add_test(tc_core, test_convert_VI_to_6);
 	tcase_add_test(tc_core, test_convert_IX_to_9);
 	tcase_add_test(tc_core, test_convert_X_to_10);
+	tcase_add_test(tc_core, test_convert_XI_to_11);
+	tcase_add_test(tc_core, test_convert_XIV_to_14);
+	tcase_add_test(tc_core, test_convert_XV_to_15);
+	tcase_add_test(tc_core, test_convert_XVI_to_16);
 	tcase_add_test(tc_core, test_convert_XL_to_40);
+	tcase_add_test(tc_core, test_convert_L_to_50);
+	tcase_add_test(tc_core, test_convert_C_to_100);
+	tcase_add_test(tc_core, test_convert_D_to_500);
+	tcase_add_test(tc_core, test_convert_M_to_1000);
+	tcase_add_test(tc_core, test_convert_MMMCMXCIX_to_3999);
 
 	suite_add_tcase(s, tc_core);
 
