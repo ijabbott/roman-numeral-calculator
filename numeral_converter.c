094972692d6a60d71adf2bcf8ccd0750ee79c4e5
Ian Abbott <ij.abbott2306@gmail.com>
2018-10-12 06:06:40 -0500

    Refactoring valid numeral check to separate function

--- numeral_converter.c
+++ numeral_converter.c
@@ -3,35 +3,22 @@
 #include <stdio.h>
 #define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
 
+static int is_numeral_valid(char *numeral);
+
 const int decimal_list[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 const char *numeral_list[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-const char valid_numerals[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I', '\0'};
+const char valid_characters[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I', '\0'};
 
 void numeral_to_decimal(char *numeral, int *decimal)
 {	
 	int list_index;
 	int numeral_index = 0;
-	int i;
-	int previous_value = 1000;
 
-	if(strstr(numeral, "IIII") != NULL ||
-	   strstr(numeral, "XXXX") != NULL ||
-	   strstr(numeral, "CCCC") != NULL ||
-	   strstr(numeral, "VV")   != NULL ||
-	   strstr(numeral, "LL")   != NULL ||
-	   strstr(numeral, "DD")   != NULL)
+	if(!is_numeral_valid(numeral))
 	{
 		return;
 	}
-	
-	for(i = 0; i < strlen(numeral); i++)
-	{
-		if(strchr(valid_numerals, numeral[i]) == NULL)
-		{
-			return;
-		}
-	}
-	
+
 	// Compare numeral character/s with the numeral list until there is a match, then shift the numeral index
 	while(numeral_index < strlen(numeral))
 	{
@@ -39,18 +26,9 @@ void numeral_to_decimal(char *numeral, int *decimal)
 		{
 			if(strncmp(&numeral[numeral_index], numeral_list[list_index], strlen(numeral_list[list_index])) == 0)
 			{
-				if(decimal_list[list_index] <= previous_value)
-				{
-					*decimal += decimal_list[list_index];
-					previous_value = decimal_list[list_index];
-					numeral_index += strlen(numeral_list[list_index]);
-					break;
-				}
-				else
-				{
-					*decimal = 0;
-					return;
-				}
+				*decimal += decimal_list[list_index];
+				numeral_index += strlen(numeral_list[list_index]);
+				break;
 			}
 		}
 	}
@@ -69,4 +47,49 @@ void decimal_to_numeral(int decimal, char *numeral)
 			decimal -= decimal_list[list_index];
 		}
 	}
+}
+
+static int is_numeral_valid(char *numeral)
+{
+	int list_index;
+	int numeral_index = 0;
+	int i;
+	int previous_value = decimal_list[numeral_index];
+
+	for(i = 0; i < strlen(numeral); i++)
+	{
+		if(strchr(valid_characters, numeral[i]) == NULL)
+		{
+			return 0;
+		}
+	}
+
+	if(strstr(numeral, "IIII") != NULL ||
+	   strstr(numeral, "XXXX") != NULL ||
+	   strstr(numeral, "CCCC") != NULL ||
+	   strstr(numeral, "VV")   != NULL ||
+	   strstr(numeral, "LL")   != NULL ||
+	   strstr(numeral, "DD")   != NULL)
+	{
+		return 0;
+	}
+	
+	// Compare numeral character/s with the numeral list until there is a match, then shift the numeral index
+	while(numeral_index < strlen(numeral))
+	{
+		for(list_index = 0; list_index < NELEMS(numeral_list); list_index++)
+		{
+			if(strncmp(&numeral[numeral_index], numeral_list[list_index], strlen(numeral_list[list_index])) == 0)
+			{
+				if(decimal_list[list_index] > previous_value)
+				{
+					return 0;
+				}
+				previous_value = decimal_list[list_index];
+				numeral_index += strlen(numeral_list[list_index]);
+			}
+		}
+	}
+
+	return 1;
 }
