a0fe7a93bbb915027a6f35a1fcccc91bda895773
Ian Abbott <ij.abbott2306@gmail.com>
2018-10-11 20:43:55 -0500

    Adding more forgotten conversion tests

--- check_conversions.c
+++ check_conversions.c
@@ -20,6 +20,24 @@ START_TEST(test_convert_V_to_5)
 }
 END_TEST
 
+START_TEST(test_convert_1_to_I)
+{
+	char *numeral = malloc(2);
+	
+	decimal_to_numeral(1, numeral);
+	ck_assert_str_eq("I", numeral);
+}
+END_TEST
+
+START_TEST(test_convert_5_to_V)
+{
+	char *numeral = malloc(2);
+	
+	decimal_to_numeral(5, numeral);
+	ck_assert_str_eq("V", numeral);
+}
+END_TEST
+
 Suite * conversion_suite(void)
 {
 	Suite *s;
@@ -31,6 +49,8 @@ Suite * conversion_suite(void)
 
 	tcase_add_test(tc_core, test_convert_I_to_1);
 	tcase_add_test(tc_core, test_convert_V_to_5);
+	tcase_add_test(tc_core, test_convert_1_to_I);
+	tcase_add_test(tc_core, test_convert_5_to_V);
 	suite_add_tcase(s, tc_core);
 
 	return s;
