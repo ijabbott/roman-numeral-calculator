c89e23c29595b8dc7a5b7cf8cb47d4c7552f373e
Ian Abbott <ij.abbott2306@gmail.com>
2018-10-12 03:25:37 -0500

    Passing invalid_numeral_A test

--- numeral_converter.c
+++ numeral_converter.c
@@ -1,16 +1,25 @@
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 #define NELEMS(x) (sizeof(x) / sizeof((x)[0]))
 
 const int decimal_list[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 const char *numeral_list[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+const char valid_numerals[] = {'M', 'D', 'C', 'L', 'X', 'V', 'I', '\0'};
 
 void numeral_to_decimal(char *numeral, int *decimal)
 {	
 	int list_index;
 	int numeral_index = 0;
-	
+	int i;
+
+	for(i = 0; i < strlen(numeral); i++)
+	{
+		if(strchr(valid_numerals, numeral[i]) == NULL)
+		{
+			return;
+		}
+	}
 	// Compare numeral character/s with the numeral list until there is a match, then shift the numeral index
 	while(numeral_index < strlen(numeral))
 	{
