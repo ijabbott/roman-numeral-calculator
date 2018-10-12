01d39db8bcf3d9c05c499762063cc64264319ad8
Ian Abbott <ij.abbott2306@gmail.com>
2018-10-12 02:43:39 -0500

    Adding more subtraction tests, all passed on the first try. Fixing potential malloc bug when subtracting causes the result to be larger than the two inputs

--- check_subtractions.c
+++ check_subtractions.c
@@ -3,20 +3,40 @@
 #include "check_subtractions.h"
 #include "numeral_calculator.h"
 
+#define MAX_LENGTH 12 // MMMCCCXXXIII
+
 /* Cases Covered
-	
+	- Subtracting numerals that contain all of the same character
+	- Subtracting numerals that contain different characters
+	- Subtracting from a numeral that will cause it to expand into smaller numerals
 */
 
 START_TEST(test_subtract_II_I)
 {
-	char *difference = malloc(strlen("II") + strlen("I") + 1);
+	char *difference = malloc(MAX_LENGTH);
 	subtract_numerals("II", "I", difference);
 	ck_assert_str_eq("I", difference);
 	free(difference);
 }
 END_TEST
 
+START_TEST(test_subtract_XV_VIII)
+{
+	char *difference = malloc(MAX_LENGTH);
+	subtract_numerals("XV", "VIII", difference);
+	ck_assert_str_eq("VII", difference);
+	free(difference);
+}
+END_TEST
 
+START_TEST(test_subtract_XL_I)
+{
+	char *difference = malloc(MAX_LENGTH);
+	subtract_numerals("XL", "I", difference);
+	ck_assert_str_eq("XXXIX", difference);
+	free(difference);
+}
+END_TEST
 
 Suite * subtractions_suite(void)
 {
@@ -28,6 +48,8 @@ Suite * subtractions_suite(void)
 	tc_core = tcase_create("Core");
 
 	tcase_add_test(tc_core, test_subtract_II_I);
+	tcase_add_test(tc_core, test_subtract_XV_VIII);
+	tcase_add_test(tc_core, test_subtract_XL_I);
 
 	suite_add_tcase(s, tc_core);
 
