a3b890c2b25b723e72c5c945d10fe3caef0bc5d3
Ian Abbott <ij.abbott2306@gmail.com>
2018-10-12 11:16:05 -0500

    Refactoring calculation to remove duplicate code

--- numeral_calculator.c
+++ numeral_calculator.c
@@ -6,44 +6,51 @@
 #define MIN_VALUE 1
 #define MAX_VALUE 3999
 
+typedef enum Operation_E
+{
+	ADDITION,
+	SUBTRACTION
+} Operation_T;
+
+static int calculate_numerals(char *operand1, char *operand2, char *numeral_result, Operation_T op);
+
 int add_numerals(char *operand1, char *operand2, char *sum)
 {
-	int result = 1;
-	int decimal_sum = 0;
-	int decimal1 = 0;
-	int decimal2 = 0;
-
-	if(numeral_to_decimal(operand1, &decimal1) == 0 && numeral_to_decimal(operand2, &decimal2) == 0)
-	{
-		decimal_sum = decimal1 + decimal2;
-
-		if(decimal_sum < MAX_VALUE)
-		{
-			decimal_to_numeral(decimal_sum, sum);
-			result = 0;
-		}
-	}
-
-	return result;
+	return calculate_numerals(operand1, operand2, sum, ADDITION);
 }
 
 int subtract_numerals(char *operand1, char *operand2, char *difference)
 {
+	return calculate_numerals(operand1, operand2, difference, SUBTRACTION);
+}
+
+static int calculate_numerals(char *operand1, char *operand2, char *numeral_result, Operation_T op)
+{
 	int result = 1;
-	int decimal_difference = 0;
+	int decimal_result = 0;
 	int decimal1 = 0;
 	int decimal2 = 0;
 
 	if(numeral_to_decimal(operand1, &decimal1) == 0 && numeral_to_decimal(operand2, &decimal2) == 0)
 	{
-		decimal_difference = decimal1 - decimal2;
-
-		if(decimal_difference >= MIN_VALUE && decimal1 < MAX_VALUE && decimal2 < MAX_VALUE)
+		if(decimal1 >= MIN_VALUE && decimal1 <= MAX_VALUE)
 		{
-			decimal_to_numeral(decimal_difference, difference);
-			result = 0;
+			if(op == ADDITION)
+			{
+				decimal_result = decimal1 + decimal2;
+			}
+			else if(op == SUBTRACTION)
+			{
+				decimal_result = decimal1 - decimal2;
+			}
+
+			if(decimal_result >= MIN_VALUE && decimal_result <= MAX_VALUE)
+			{
+				decimal_to_numeral(decimal_result, numeral_result);
+				result = 0;
+			}
 		}
 	}
 
-	return result;	
+	return result;
 }
